ef3dd19b08539b08784d08b7bae8d59e55c8a2cb
Paul DiLoreto <paul.diloreto@gmail.com>
2024-10-20 12:22:53 -0700

    feat: kavita plugin wip

---
+++ plugins/kavita.koplugin/_meta.lua
@@ -0,0 +1,8 @@
+local _ = require("gettext")
+return {
+    name = "kavita",
+    fullname = _("Kavita"),
+    description = _(
+        [[Connects with a Kavita instance to view & download books, sync reading progress, and continue to works offline.]]
+    ),
+}
